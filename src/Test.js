dbca5ab5c256954fb1fcb9a456fd6ef76a881688
Arijit Das <59313848+Arijit-Das-07@users.noreply.github.com>
2021-03-03 22:21:41 +0530

    Bootstrap Added

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,9 @@
 import "./styles.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 export default function Test() {
   return (
     <div className="Test">
+      <button className="btn btn-success"> Good </button>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
